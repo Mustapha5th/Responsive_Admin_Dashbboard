240af7d264ae2f1f8261b54c8e6b10390e5a1d02
Mustapha Idris Suleiman <mustapha5th@gmail.com>
2022-03-05 13:02:26 +0100

    first comit

---
+++ js/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Dominos Pizza",
+    productNumber: "13234",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Grilled Chicken",
+    productNumber: "42112",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Italian Spagetti",
+    productNumber: "31211",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Fried Rice",
+    productNumber: "53322",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Beef Shawarma",
+    productNumber: "53122",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Indomie Noodle",
+    productNumber: "12393",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  }
+];
